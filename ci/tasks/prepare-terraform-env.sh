9336754a0f7343d8d6aa1cf922a5bbffc0febc92
dmutreja <dhiraj.mutreja@oracle.com>
2018-04-08 18:43:00 -0700

    Restrict network security list to required ports only.

--- ci/tasks/prepare-terraform-env.sh
+++ ci/tasks/prepare-terraform-env.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e
 
@@ -17,17 +17,7 @@ EOF
 chmod 600 ${api_key_path}
 
 cat > ${vars_file} <<EOF
-oracle_user_ocid: ${oracle_user}
-oracle_tenancy_ocid: ${oracle_tenancy}
-oracle_region: ${oracle_region}
 oracle_private_key_path: ${api_key_path}
-oracle_fingerprint: ${oracle_fingerprint}
-director_compartment_name: ${director_compartment_name}
-director_vcn: ${director_vcn}
-director_subnet_cidr: ${director_subnet_cidr}
-director_ad: ${director_ad}
-bats_subnet1_cidr: ${bats_subnet1_cidr}
-bats_subnet2_cidr: ${bats_subnet2_cidr}
 EOF
 
 echo "Done. Created: " ${vars_file}
